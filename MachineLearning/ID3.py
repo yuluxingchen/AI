fb1f50750ba484a3cb710f4927a979781c1caecf
szj <858767671@qq.com>
2023-04-19 13:56:23 +0800

    修改了ID3训练集数据和部分代码

--- MachineLearning/ID3.py
+++ MachineLearning/ID3.py
@@ -1,120 +1,157 @@
 # 3代迭代二叉树
-import pandas as pd
 from math import log
-from sklearn.datasets import load_wine
 
-
-def entropy(list):
-    """
-    信息熵计算函数
-    :param list: 包含类别取值的列表
-    :return: 信息熵值
-    """
-    # 计算列表中取值的概率分布
-    probs = [list.count(i) / len(list) for i in set(list)]
-    # 计算信息熵
-    entropy = -sum([prob * log(prob, 2) for prob in probs])
-    return entropy
-
-
-def df_split(df, col):
-    """
-    根据数据集和指定特征定义数据集划分函数
-    :param df: 待划分的训练数据
-    :param col: 划分数据的依据特征
-    :return: 根据特征取值划分后的不同数据集字典
-    """
-    # 获取依据特征的不同取值
-    unique_col_val = df[col].unique()
-    # 创建划分结果的数据框字典
-    res_dict = {elem: pd.DataFrame for elem in unique_col_val}
-    # 根据特征取值进行划分
-    for key in res_dict.keys():
-        res_dict[key] = df[:][df[col] == key]
-    return res_dict
-
-
-def choose_best_feature(df, label):
-    """
-    根据训练集和标签选择信息增益最大的特征作为最优特征
-    :param df: 待划分的训练数据
-    :param label: 训练标签
-    :return:
-    max_value: 最大信息增益值      <br>
-    best_feature: 最优特征        <br>
-    max_splited: 根据最优特征划分后的数据字典 <br>
-    """
-    # 计算训练标签的信息熵
-    entropy_D = entropy(df[label].tolist())
-    # 特征集
-    cols = [col for col in df.columns if col not in [label]]
-    # 初始化最大信息增益值、最优特征和划分后的数据集
-    max_value, best_feature, max_splited = -999, None, None
-    # 遍历特征并根据特征取值进行划分
-    for col in cols:
-        # 根据当前特征划分后的数据集
-        splited_set = df_split(df, col)
-        # 初始化经验条件熵
-        entropy_DA = 0
-        # 对划分后的数据集遍历计算
-        for subset_col, subset in splited_set.items():
-            # 计算划分后的数据子集的标签信息熵
-            entropy_Di = entropy(subset[label].tolist())
-            # 计算当前特征的经验条件熵
-            entropy_DA += len(subset) / len(df) * entropy_Di
-        # 计算当前特征的信息增益
-        info_gain = entropy_D - entropy_DA
-        # 获取最大信息增益，并保存对应的特征和划分结果
-        if info_gain > max_value:
-            max_value, best_feature = info_gain, col
-            max_splited = splited_set
-    return max_value, best_feature, max_splited
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 
 class ID3Tree:
     class TreeNode:
-        # 定义树结点
-        def __init__(self, name):
-            self.name = name
-            self.connections = {}
+        def __init__(self, feature=None, value=None, result=None):
+            # 特征编号
+            self.feature = feature
+            # 特征取值
+            self.value = value
+            # 如果是叶子节点，代表分类结果
+            self.result = result
+            # 子节点
+            self.son = {}
 
-        # 定义树连接
-        def connect(self, label, node):
-            self.connections[label] = node
+    def __init__(self):
+        self.root = None
 
-    # 定义全局变量，包括数据集、特征集、标签和根节点
-    def __init__(self, df, label):
-        self.columns = df.columns
-        self.df = df
-        self.label = label
-        self.root = self.TreeNode("Root")
+    def choose_best_feature(self, X, y):
+        n, m = X.shape
 
-    # 构建树的调用
-    def construct_tree(self):
-        self.construct(self.root, "", self.df, self.columns)
+        # 计算标签的信息熵
+        base_entropy = self.entropy(y)
+        # 初始化最大信息增益值、最优特征和划分后的数据集
+        max_info_gain, best_feature = -999, -1
 
-    # 决策树构建方法
-    def construct(self, parent_node, parent_label, sub_df, columns):
-        # 选择最优特征
-        max_value, best_feature, max_splited = choose_best_feature(sub_df[columns], self.label)
-        # 如果选不到最优特征，则构造单结点树
-        if not best_feature:
-            node = self.TreeNode(sub_df[self.label].iloc[0])
-            parent_node.connect(parent_label, node)
-            return
-        node = self.TreeNode(best_feature)
-        parent_node.connect(parent_label, node)
-        # 以A-Ag为新的特征集
-        new_columns = [col for col in columns if col != best_feature]
-        # 递归地构造决策树
-        for splited_value, splited_data in max_splited.items():
-            self.construct(node, splited_value, splited_data, new_columns)
+        for i in range(m):
+            # 获取此特征下的所有取值
+            feature_values = set(X[:, i])
+            new_entropy = 0.0
+            # 对每一个取值依次进行条件信息熵计算
+            for value in feature_values:
+                # 获取取值的行坐标
+                index = np.where(X[:, i] == value)[0]
+                sub_y = y[index]
+                # 计算取值占比
+                prob = float(len(index)) / n
+                # 获得信息熵
+                new_entropy += prob * self.entropy(sub_y)
+            # 计算信息增益
+            info_gain = base_entropy - new_entropy
+            # 保留信息增益最大的特征
+            if info_gain > max_info_gain:
+                max_info_gain = info_gain
+                best_feature = i
+        return best_feature
 
-    def show(self):
-        print(self.root.name)
-        for i in self.root.connections:
-            print(i)
+    def construct(self, X, y):
+        """
+        生成特征树结构
+        :param X: 样本集
+        :param y: 样本集的标签
+        :return:
+        """
+        # 如果样本中所有类别相同，则直接返回
+        if len(set(y)) == 1:
+            return self.TreeNode(result=y[0])
+        # 如果没有特征可用，则选择出现最多的结果作为分类结果
+        if len(X[0]) == 0:
+            return self.TreeNode(result=max(y, key=y.count))
+        # 选择信息增益最大的特征作为分割点
+        best_feature = self.choose_best_feature(X, y)
+        # 以此特征作为特征根建立特征树
+        root = self.TreeNode(feature=best_feature)
+        # 此特征下的特征值集
+        values = set(X[:, best_feature])
+        for val in values:
+            # 获取满足某一个特征值的样本集行坐标
+            idx = np.where(X[:, best_feature] == val)[0]
+            # 对这些样本进行进一步划分
+            root.son[val] = self.construct(X[idx], y[idx])
+        return root
+
+    def entropy(self, y):
+        """
+        信息熵计算函数
+        :param target: 标签列表
+        :param labels: 标签类别列表
+        :return: 信息熵值
+        """
+        # 特征集
+        labels = np.unique(y)
+        # 计算各个标签的概率分布
+        probs = [np.sum(y == i) / len(y) for i in labels]
+        # 计算信息熵
+        entropy_probs = - sum([prob * log(prob, 2) for prob in probs])
+        return entropy_probs
+
+    def fit(self, X, y):
+        self.root = self.construct(X, y)
+
+    def predict_one(self, root, X):
+        """
+        预测单个样本类别
+        :param root: 特征根节点
+        :param X: 待预测样本
+        :return: 样本类别
+        """
+        # 如果此特征节点存在结果，则直接返回
+        if root.result is not None:
+            return root.result
+        # 获取此特征节点的特征编号
+        feature = X[root.feature]
+        # 如果此特征不属于子特征节点则返回未知
+        if feature not in root.son:
+            return -1
+        # 从子特征节点中继续寻找
+        son = root.son[feature]
+        return self.predict_one(son, X)
+
+    def predict(self, X):
+        """
+        预测样本集中所有样本类别
+        :param X: 待预测样本集
+        :return: 样本类别
+        """
+        # 返回结果
+        result = []
+        # 对样本集中的每一个样本单独进行预测
+        for i in range(len(X)):
+            result.append(self.predict_one(self.root, X[i]))
+        return np.array(result)
 
 
 if __name__ == '__main__':
-    pass
+    data = load_iris()
+    # 将数据集分为训练集和测试集
+    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3, random_state=42)
+    # 创建ID3树
+    id3 = ID3Tree()
+    # 拟合数据
+    id3.fit(X_train, y_train)
+    # 预测数据
+    y_pred = id3.predict(X_test)
+    # 精确率
+    accuracy = sum([y_pred[i] == y_test[i] for i in range(len(y_test))]) / len(y_test)
+    print(accuracy)
+
+    # 不同类别颜色设置
+    colors = ['navy', 'turquoise', 'darkorange']
+    # 绘制预测分类
+    for c, i, target_name in zip(colors, [0, 1, 2], data.target_names):
+        plt.scatter(X_test[y_pred == i, 0], X_test[y_pred == i, 1], color=c, lw=2, label=target_name)
+    plt.legend()
+    plt.show()
+
+    # 绘制测试集分类
+    for c, i, target_name in zip(colors, [0, 1, 2], data.target_names):
+        plt.scatter(X_test[y_test == i, 0], X_test[y_test == i, 1], color=c, lw=2, label=target_name)
+    plt.legend()
+    plt.show()
