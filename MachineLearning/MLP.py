bccdb674020cb9907816a5ae9ed062a3161e91ee
szj <858767671@qq.com>
2023-04-21 17:00:27 +0800

    重写MLP算法

--- MachineLearning/MLP.py
+++ MachineLearning/MLP.py
@@ -1,75 +1,115 @@
-import torch
-import torchvision.datasets
-from torch import nn
-from torch.utils import data
-from torchvision import transforms
+import numpy as np
 
 
-def get_dataloader_workers():
+def sigmoid(x):
     """
-    获取读取数据的进程数
-    :return: 读取数据的进程数
+    定义sigmoid函数
+    :param x: 输入数据
+    :return: 计算后的数据
     """
-    return 4
+    return 1.0 / (1 + np.exp(-x))
 
 
-def load_data(batch_size, resize=None):
+def initialize_parameters(input_dim, output_dim):
+    w = np.random.normal(size=(output_dim, input_dim))
+    b = np.random.normal(size=(output_dim, 1))
+    return w, b
+
+
+def cost(A, Y):
+    m = Y.shape[0]
+    log_probs = np.multiply(np.log(A), Y) + np.multiply(np.log(1 - A), (1 - Y))
+    cost = - 1 / m * np.sum(log_probs)
+    return cost
+
+
+class MLP:
+    def __init__(self, input_dim, hidden_dim, output_dim, layer=2):
+        self.layer_parameters = []
+        self.cache = {}
+        for i in range(layer):
+            if i == 0:
+                w, b = initialize_parameters(input_dim, hidden_dim)
+            elif 0 < i < layer - 1:
+                w, b = initialize_parameters(hidden_dim, hidden_dim)
+            else:
+                w, b = initialize_parameters(hidden_dim, output_dim)
+            self.layer_parameters.append([w, b])
+
+    def forward_propagation(self, X):
+        for index in range(len(self.layer_parameters)):
+            w, b = self.layer_parameters[index]
+            A = sigmoid(np.dot(X, w.T) + b.T)
+            self.cache[index] = [X, A]
+            X = A
+        return X
+
+    def backward_propagation(self, X, Y):
+        grads = [[] for _ in range(len(self.cache))]
+        for i in range(len(self.cache) - 1, 0, -1):
+            m = X.shape[0]
+            x, a = self.cache[i]
+            w, b = self.layer_parameters[i]
+            if i == len(self.cache) - 1:
+                diff = np.dot(Y, (a - Y) * x * (1 - x))
+            else:
+                diff = np.dot(x, np.dot(a - Y, w.T) * a * (1 - a))
+            dw = 1 / m * np.dot(x.T, a)
+            db = 1 / m * np.sum(diff, axis=0, keepdims=True)
+            grads[i] = [dw, db]
+        return grads
+
+    def update_parameters(self, grads, learning_rate=0.5):
+        for index in range(len(self.layer_parameters) - 1, 0, -1):
+            w, b = self.layer_parameters[index]
+            dw, db = grads[index]
+            w += dw.T * learning_rate
+            b += db.T * learning_rate
+            self.layer_parameters[index] = [w, b]
+
+    def train(self, X, Y, learning_rate=0.5, epochs=100):
+        for i in range(epochs):
+            A = self.forward_propagation(X)
+            loss = cost(A, Y)
+            grads = self.backward_propagation(X, Y)
+            self.update_parameters(grads, learning_rate)
+            if i % 100 == 0:
+                print("Cost after iteration %i:%f" % (i, loss))
+
+
+# 生成非线性可分数据集
+def create_dataset():
     """
-    下载 Fashion-MNIST数据集， 然后将其加载到内存中
-    :param batch_size: 批处理大小
-    :param resize: 调整后图像大小
-    :return: 加载后的数据集
+    :return:
+    X：模拟数据集输入 <br>
+    Y：模拟数据集输出
     """
-    # 声明一个操作列表。列表中包含 ToTensor 步骤
-    # ToTensor 实例可将图像数据从 PIL 类型变换成 32 位浮点数
-    trans = [transforms.ToTensor()]
-    if resize:
-        # Resize 用于调整图像大小
-        # 将 Resize 步骤放在操作列表的第一位
-        trans.insert(0, transforms.Resize(resize))
-    # 用来组合多个 transforms 操作
-    # 根据操作列表中放入的操作顺序依次执行
-    trans = transforms.Compose(trans)
-    mnist_train = torchvision.datasets.FashionMNIST(
-        root="/data", train=True, transform=trans, download=True
-    )
-    mnist_test = torchvision.datasets.FashionMNIST(
-        root="/data", train=False, transform=trans, download=True
-    )
-    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
-                            num_workers=get_dataloader_workers()),
-            data.DataLoader(mnist_test, batch_size, shuffle=False,
-                            num_workers=get_dataloader_workers()))
-
-
-def init_params(num_inputs, num_outputs, num_hiddens):
-    W1 = nn.Parameter(torch.randn(num_inputs, num_hiddens, requires_grad=True) * 0.01)
-    b1 = nn.Parameter(torch.zeros(num_hiddens, requires_grad=True))
-    W2 = nn.Parameter(torch.randn(num_hiddens, num_outputs, requires_grad=True) * 0.01)
-    b2 = nn.Parameter(torch.zeros(num_outputs, requires_grad=True))
-    params = [W1, b1, W2, b2]
-    return params
-
-
-def relu(X):
-    a = torch.zeros_like(X)
-    return torch.max(X, a)
-
-
-def net(X, params, num_inputs):
-    X = X.reshape((-1, num_inputs))
-    H = relu(torch.dot(X, params.W1) + params.b1)
-    return torch.dot(H, params.W2) + params.b2
-
-
-def cross_entropy(y_hat, y):
-    return - torch.log()
+    # 设置随机数种子
+    np.random.seed(1)
+    # 数据量
+    m = 400
+    # 每个标签的实例数
+    N = int(m / 2)
+    # 数据维度
+    D = 2
+    # 数据矩阵
+    X = np.zeros((m, D))
+    # 标签维度
+    Y = np.zeros((m, 1), dtype='uint8')
+    a = 4
+    # 遍历生成数据
+    for j in range(2):
+        ix = range(N * j, N * (j + 1))
+        # theta
+        t = np.linspace(j * 3.12, (j + 1) * 3.12, N) + np.random.randn(N) * 0.2
+        # radius
+        r = a * np.sin(4 * t) + np.random.randn(N) * 0.2
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        Y[ix] = j
+    return X, Y
 
 
 if __name__ == '__main__':
-    # 单批量数据大小
-    # batch_size = 256
-    # train_iter, test_iter = load_data(batch_size)
-    a = [1,2,2,3,4,5,6]
-    b = a[range(len(a))]
-    print(b)
+    X, Y = create_dataset()
+    mlp = MLP(X.shape[1], 10, Y.shape[1])
+    mlp.train(X, Y, learning_rate=0.5, epochs=10000)
